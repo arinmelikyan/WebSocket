782a9e20f08396cccd69f4f11028062701dab990
Arin Melikyan <37839859+arinmelikyan@users.noreply.github.com>
2019-01-25 18:18:06 +0400

    Update ws_server.js

--- ws-server/ws_server.js
+++ ws-server/ws_server.js
@@ -1,14 +1,6 @@
 const WebSocketServer = require("websocket").server;
-const http = require("http");
-
-const httpServer = http.createServer((req, res) => {});
-
-httpServer.listen(3000, "localhost", function() {
-    console.log("Listening on port 3000")
-});
 
 const wss = new WebSocketServer({
-    httpServer: httpServer,
     port: 3000
 });
 
